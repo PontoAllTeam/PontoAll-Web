5c237cd915ada44c08524335d804e51f97a51bcb
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-05 22:43:48 -0300

    :sparkles: feat: Criando componente Header.

---
+++ PontoAllWeb/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-white flex items-center justify-between h-16 shadow z-10 w-full">
+      <div className="px-4">
+      </div>
+    </header>
+  );
+}
