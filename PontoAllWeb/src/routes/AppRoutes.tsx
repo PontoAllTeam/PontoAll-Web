1a2a32e8c2d59ab58c45adc35d82987e71aeddfe
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-08 23:06:41 -0300

    :sparkles: feat: Adicionando rotas da página LandingPage.

--- PontoAllWeb/src/routes/AppRoutes.tsx
+++ PontoAllWeb/src/routes/AppRoutes.tsx
@@ -10,6 +10,7 @@ import { routes } from './routes';
 import LoginPage from '@/pages/LoginPage';
 import CompanyRegistration from '@/pages/Registrations/CompanyRegistration';
 import TesteLayout from "@/pages/TesteLayout";
+import LandingPage from '@/pages/LandingPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
       <Route path={routes.LOGIN} element={<LoginPage />} />
       <Route path={routes.COMPANY_REGISTRATION} element={<CompanyRegistration />}/>
       <Route path={routes.TESTE_LAYOUT} element={<TesteLayout />} />
+      <Route path={routes.LANDING_PAGE} element={<LandingPage />} />
     </Route>
   )
 );
--- PontoAllWeb/src/routes/routes.tsx
+++ PontoAllWeb/src/routes/routes.tsx
@@ -2,5 +2,6 @@ export const routes = {
   LOGIN: '/login',
   COMPANY_REGISTRATION: '/company_registration',
   TESTE_LAYOUT: '/teste_layout',
+  LANDING_PAGE: '/landing_page',
 }
 
