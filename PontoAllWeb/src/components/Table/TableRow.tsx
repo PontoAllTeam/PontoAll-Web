b32e826566c3e67fc703530db5bfa1562cc0907c
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-18 17:42:40 -0300

    :lipstick: feat: Alterando cor do texto da primeira coluna, e cor da checkbox selecionada.

--- PontoAllWeb/src/components/Table/TableRow.tsx
+++ PontoAllWeb/src/components/Table/TableRow.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+import { JSX } from 'react';
 
 interface TableRowProps {
   data: any;
@@ -8,33 +8,40 @@ interface TableRowProps {
 
 export default function TableRow({ data, index, actions }: TableRowProps) {
   const keys = Object.keys(data);
-  return(
+  return (
     <tr
       className={`border-y border-text-primary text-text-primary h-12 ${
-        index % 2 === 1 ? "bg-neutral-light" : "bg-white"
+        index % 2 === 1 ? 'bg-neutral-light' : 'bg-white'
       }`}
     >
       {/* Célula da checkbox */}
       <td>
-        <div className="h-full flex items-center justify-center">
-          <input type="checkbox" className="cursor-pointer" />
+        <div className='h-full flex items-center justify-center'>
+          <input
+            type='checkbox'
+            className='form-checkbox h-4 w-4 accent-secondary'
+          />
         </div>
       </td>
 
       {/* Conteúdo */}
-      {keys.map((value, index) => {
-        if (index > 0)
+      {keys.map((value, idx) => {
+        if (idx > 0) {
           return (
-            <td key={data[value]}>
+            <td
+              key={data[value]}
+              className={idx === 1 ? 'text-secondary font-semibold' : ''}
+            >
               {data[value]}
             </td>
-          )
+          );
+        }
       })}
 
       {/* Célula dos botões */}
       {actions && (
-        <td className="px-4 text-center border-l-1 border-text-primary h-full">
-          <div className="flex h-full justify-evenly items-center">
+        <td className='px-4 text-center border-l-1 border-text-primary h-full'>
+          <div className='flex h-full justify-evenly items-center'>
             {actions}
           </div>
         </td>
