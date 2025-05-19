d194984455bfb38a4b77a5dcc3c9bfa980191307
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-19 15:10:43 -0300

    :sparkles: feat: Adicionando modal na página UserManagement.

--- PontoAllWeb/src/pages/UserManagement/index.tsx
+++ PontoAllWeb/src/pages/UserManagement/index.tsx
@@ -1,11 +1,14 @@
 import Table from '@/components/Table';
-import SearchBar from '@/components/SearchBar'; // ajuste o path conforme seu projeto
+import SearchBar from '@/components/SearchBar';
+import UserRegisterModal from '@/components/UserRegisterModal';
 import { useState } from 'react';
 import { MdEdit, MdDelete, MdAdd } from 'react-icons/md';
 
 export default function UserManagement() {
   //Declaração de estado
   const [search, setSearch] = useState('');
+  const [openModal, setOpenModal] = useState(false);
+
   const columns = [
     'Nome Funcionário',
     'Departamento',
@@ -64,13 +67,31 @@ export default function UserManagement() {
 
   return (
     <div className='p-6'>
-      <div className='flex justify-between items-center px-4 py-2 bg-neutral-dark rounded-sm border border-text-primary mb-4'>
+      <div className='flex justify-between items-center px-4 py-2 bg-neutral-dark rounded-sm mb-4'>
         <SearchBar onChange={setSearch} />
-        <button className='ml-4 bg-secondary text-white text-sm px-8 py-2 rounded-md font-semibold shadow-md hover:bg-accent flex items-center gap-2'>
+        <button
+          onClick={() => setOpenModal(true)}
+          className='ml-4 bg-secondary text-white text-sm px-8 py-2 rounded-md font-semibold shadow-md hover:bg-accent flex items-center gap-2'
+        >
           <MdAdd size={20} />
           Cadastrar Funcionário
         </button>
       </div>
+
+      {openModal && (
+        <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50'>
+          <div className='bg-white rounded-lg shadow-lg p-6 w-full max-w-5xl'>
+            <UserRegisterModal />
+            <button
+              onClick={() => setOpenModal(false)}
+              className='mt-4 text-sm text-red-600 hover:underline'
+            >
+              Fechar
+            </button>
+          </div>
+        </div>
+      )}
+
       <Table columns={columns} data={formattedData} actions={actions} />
     </div>
   );
