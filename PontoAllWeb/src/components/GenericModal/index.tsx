39fd6482fe9bbfcbca134166e503716e1a068151
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-20 17:20:00 -0300

    :sparkles: feat: Criando componente GenericModal.

---
+++ PontoAllWeb/src/components/GenericModal/index.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+
+interface InputProps {
+  label: string;
+  value?: string;
+  description?: string;
+  action: (value: string) => void;
+}
+
+function Input({ label, value, action }: InputProps) {
+  return (
+    <div className="mb-4">
+      <label className="block text-text-primary text-sm mb-1 break-all">{label}:</label>
+      <input
+        type="text"
+        className="w-full py-2 pl-4 text-sm text-text-primary rounded border border-gray-100 focus:outline-none focus:border-gray-300"
+        value={value}
+        onChange={(e) => {
+          action(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+interface ModalProps {
+  title?: string;
+  inputs?: { label: string; description?: string }[];
+  description?: string;
+  action?: (data: { [key: string]: string }) => void;
+  statusModal?: boolean;
+}
+
+export default function Modal({title = "Título", inputs = [], action, statusModal = false}: ModalProps) {
+  const [formData, setFormData] = useState({});
+  const [showModal, setShowModal] = useState(statusModal);
+
+  const handleFormSubmit = (label: string, value: string) => {
+    setFormData((prev) => ({ ...prev, [label]: value }));
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = () => {
+    if (action) {
+      action(formData);
+    }
+  };
+
+  if (!showModal)
+    return null;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+      <form className="bg-white rounded-lg shadow-lg w-full max-w-lg p-4">
+        {/* Cabeçalho do Modal */}
+        <div className="flex justify-center items-center px-4 py-0 mr-0 border-0">
+          <h2 className="text-xl font-semibold ">{title}</h2>
+        </div>
+
+        {/* Separador */}
+        <hr className="border-t border-gray-300 w-5/6 mx-auto my-4" />
+
+        {/* Corpo do Modal */}
+        <div>
+          {inputs.map((input) => (
+            <Input
+              key={input.label}
+              label={input.label}
+              description={input.description}
+              action={(value) => handleFormSubmit(input.label, value)}
+            />
+          ))}
+        </div>
+
+        {/* Separador */}
+        <hr className="border-t border-gray-300 w-5/6 mx-auto my-4" />
+
+        {/* Rodapé do Modal */}
+        <div className="flex justify-end px-4 py-2">
+          <button
+            type="button"
+            className="bg-neutral-light text-text-primary px-5 py-1 rounded hover:bg-neutral-dark transition"
+            onClick={closeModal}
+          >
+            Cancelar
+          </button>
+          <button
+            type="button"
+            className="bg-secondary text-white px-5 py-1 rounded hover:bg-accent transition ml-3"
+            onClick={handleSubmit}
+          >
+            Concluir
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
