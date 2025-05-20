bc7115eb2a5c2dcf811902b147d78c05a33a9ed8
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-05-20 16:25:47 -0300

    :lipstick: feat: Adicionando Breadcrumb_PageTitle.

--- PontoAllWeb/src/pages/UserManagement/index.tsx
+++ PontoAllWeb/src/pages/UserManagement/index.tsx
@@ -4,6 +4,7 @@ import UserRegisterModal from '@/components/UserRegisterModal';
 import { useState } from 'react';
 import { MdEdit, MdDelete, MdAdd } from 'react-icons/md';
 import Button from '@/components/Button';
+import Breadcrumb_PageTitle from '@/components/BreadcrumbPageTitle';
 
 export default function UserManagement() {
   //Declaração de estado
@@ -67,25 +68,39 @@ export default function UserManagement() {
   }));
 
   return (
-    <div className='p-6'>
-      <div className='flex justify-between items-center px-4 py-2 bg-neutral-dark rounded-sm mb-4'>
-        <SearchBar onChange={setSearch} />
-        <Button label="Cadastrar Funcionário" color="secondary" size="sm" icon={<MdAdd size={20} />} onClick={() => setOpenModal(true)}/>
-      </div>
+    <div className='w-full'>
+      <Breadcrumb_PageTitle title="Funcionários" />
+      <div className='p-6'>
+        <div className='flex justify-between items-center px-4 py-2 bg-neutral-dark rounded-sm mb-4'>
+          <SearchBar onChange={setSearch} />
+          <Button
+            label='Cadastrar Funcionário'
+            color='secondary'
+            size='sm'
+            icon={<MdAdd size={20} />}
+            onClick={() => setOpenModal(true)}
+          />
+        </div>
 
-      {openModal && (
-        <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50'>
-          <div className='bg-white rounded-lg shadow-lg p-6 max-h-[90%] overflow-auto w-full max-w-5xl'>
-            <UserRegisterModal />
-            <div className='flex justify-end gap-4 py-2'>
-              <Button label="Cancelar" color="cancel" size="md" onClick={() => setOpenModal(false)}/>
-              <Button label="Cadastrar" color="secondary" size="lg"/>
+        {openModal && (
+          <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50'>
+            <div className='bg-white rounded-lg shadow-lg p-6 max-h-[90%] overflow-auto w-full max-w-5xl'>
+              <UserRegisterModal />
+              <div className='flex justify-end gap-4 py-2'>
+                <Button
+                  label='Cancelar'
+                  color='cancel'
+                  size='md'
+                  onClick={() => setOpenModal(false)}
+                />
+                <Button label='Cadastrar' color='secondary' size='lg' />
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      <Table columns={columns} data={formattedData} actions={actions} />
+        <Table columns={columns} data={formattedData} actions={actions} />
+      </div>
     </div>
   );
 }
