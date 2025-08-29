aabd43e24c77bcdcdf7c868ab1bcad0f9c1a899b
Larksel <lemuelgames@outlook.com>
2025-08-29 14:54:10 -0300

    :recycle: refactor: Correção do tipo de WorkSchedule

--- PontoAllWeb/src/types/models/workSchedule.ts
+++ PontoAllWeb/src/types/models/workSchedule.ts
@@ -7,14 +7,14 @@ export interface WorkSchedule {
   dayType: ScheduleDayType;
   markTime1: Date;
   markTime2: Date;
-  markTime3: Date;
-  markTime4: Date;
-  markTime5: Date;
-  markTime6: Date;
-  markTime7: Date;
-  markTime8: Date;
-  markTime9: Date;
-  markTime10: Date;
+  markTime3?: Date;
+  markTime4?: Date;
+  markTime5?: Date;
+  markTime6?: Date;
+  markTime7?: Date;
+  markTime8?: Date;
+  markTime9?: Date;
+  markTime10?: Date;
   userId: number;
   geofenceId: number;
 }
