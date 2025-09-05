6b2c3b8b94dc8348a7f1eddbeae5b576962446e9
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-09-05 16:29:14 -0300

    :lipstick: feat: Atualizando componentes relacionados ao título das páginas

--- PontoAllWeb/src/components/BreadcrumbPageTitle/index.tsx
+++ PontoAllWeb/src/components/BreadcrumbPageTitle/index.tsx
@@ -9,7 +9,7 @@ export default function Breadcrumb_PageTitle({
   title,
 }: BreadcrumbPageTitleProps) {
   return (
-    <div className="bg-neutral-light p-4 px-6 shadow">
+    <div className="p-4 px-6">
       <Breadcrumb />
       <PageTitle title={title} />
     </div>
--- PontoAllWeb/src/components/PageTitle/index.tsx
+++ PontoAllWeb/src/components/PageTitle/index.tsx
@@ -5,7 +5,7 @@ interface PageTitleProps {
 export default function PageTitle({ title }: PageTitleProps) {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-text-secondary">{title}</h1>
+      <h1 className="text-xl font-bold text-text-secondary">{title}</h1>
     </div>
   );
 }
