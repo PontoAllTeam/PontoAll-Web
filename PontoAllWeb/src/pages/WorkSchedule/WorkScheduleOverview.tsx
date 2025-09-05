b786ffd5916bea96aca0b8d9bb597c606eb2a989
Larksel <lemuelgames@outlook.com>
2025-09-04 22:59:02 -0300

    :construction: Progresso no calendário da Escala

--- PontoAllWeb/src/pages/WorkSchedule/WorkScheduleOverview.tsx
+++ PontoAllWeb/src/pages/WorkSchedule/WorkScheduleOverview.tsx
@@ -4,7 +4,16 @@ import { ScheduleDayType } from '@/types/enums';
 import { WorkSchedule } from '@/types/models';
 import { PiUserFill } from 'react-icons/pi';
 
-export default function WorkScheduleOverview() {
+const EmployeeName = () => {
+  return (
+    <div className='flex flex-row items-center justify-center shrink-0 gap-2'>
+      <PiUserFill className='text-text-secondary text-2xl' />
+      <p className='text-text-secondary font-medium'>Nome funcionário</p>
+    </div>
+  );
+};
+
+const EmployeeSchedule = () => {
   const dataAgora = new Date();
   const emptySchedule: WorkSchedule = {
     dayOfMonth: 4,
@@ -17,36 +26,51 @@ export default function WorkScheduleOverview() {
     yearMonth: '2025/09',
   };
 
+  return (
+    <div className='flex flex-row gap-3 justify-between'>
+      {Array.from({ length: 7 }).map((_, index) => (
+        <ScheduleCard
+          key={index}
+          workSchedule={{
+            ...emptySchedule,
+            dayType: Math.floor(Math.random() * (6 - 1 + 1)) + 1,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default function WorkScheduleOverview() {
   const dias = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'];
 
   return (
-    <div className='w-full'>
+    <div>
       <BreadcrumbPageTitle title='Escala de Trabalho' />
+      {/* Container da página */}
       <div className='p-11 h-full'>
-        <table className='bg-white w-full h-40 border-collapse p-6 rounded-2xl'>
-          <thead className='text-text-primary font-medium text-lg'>
-            <tr className='h-12'>
-              {/* Coluna reservada para as checkbox */}
-              <th className='text-left border pt-10'>Funcionário</th>
-              {dias.map((column, index) => (
-                <th key={index} className='text-left border pt-10 pb-4'>
-                  {column}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <div className='flex flex-row items-center gap-2 py-5'>
-            <PiUserFill className='text-text-secondary text-2xl' />
-            <p className='text-text-secondary font-medium shrink-0'>
-              Nome funcionário
-            </p>
-            <div className='flex justify-between w-full'>
-              {Array.from({ length: 7 }).map(() => (
-                <ScheduleCard workSchedule={emptySchedule} />
-              ))}
+        {/* Container do calendário das escalas */}
+        <div className='bg-white p-6 rounded-2xl h-full max-h-fit flex flex-col'>
+          {/* Conteúdo do calendário */}
+          <div className='flex gap-4 items-center w-full'>
+            {/* //TODO Quando for feito a integração com a API, as duas listas (funcionários e os cards dos dias) devem estar em sincronia*/}
+            {/* Coluna dos funcionários */}
+            <div className='shrink-0 h-full flex flex-col *:my-auto'>
+              <EmployeeName />
+              <EmployeeName />
+              <EmployeeName />
+              <EmployeeName />
+            </div>
+
+            {/* Container dos cards de todos os funcionários */}
+            <div className='flex flex-col w-full overflow-x-auto *:py-5'>
+              <EmployeeSchedule />
+              <EmployeeSchedule />
+              <EmployeeSchedule />
+              <EmployeeSchedule />
             </div>
           </div>
-        </table>
+        </div>
       </div>
     </div>
   );
