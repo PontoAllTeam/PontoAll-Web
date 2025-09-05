c364c9950ae5143a9b42437929cf40ca0f4b0183
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-09-05 16:32:37 -0300

    :lipstick: feat: Atualizando as cores relacionadas ao componente button

--- PontoAllWeb/src/components/Button/index.tsx
+++ PontoAllWeb/src/components/Button/index.tsx
@@ -8,7 +8,7 @@ interface ButtonProps
   label: string;
   icon?: JSX.Element;
   iconPosition?: "left" | "right";
-  color?: "secondary" | "cancel";
+  color?: "secondary" | "cancel" | "white";
   size?: "sm" | "md" | "lg" | "xl";
 }
 
@@ -24,10 +24,11 @@ export default function Button({
   const colorClasses = {
     secondary: "bg-secondary hover:bg-accent text-white",
     cancel: "bg-neutral-light text-text-primary hover:bg-neutral-dark ",
+    white: "bg-white text-text-primary hover:bg-neutral-dark ",
   };
 
   const sizeClasses = {
-    sm: "px-6 py-2 text-sm",
+    sm: "px-2 py-2 text-sm",
     md: "px-8 py-2 text-md",
     lg: "px-14 py-2 text-md",
     xl: "px-20 py-2 text-lg",
@@ -35,7 +36,7 @@ export default function Button({
 
   return (
     <button
-      className={`flex h-fit items-center justify-center rounded-sm transition font-semibold ${icon && 'gap-1'} ${colorClasses[color]} ${sizeClasses[size]} ${className}`}
+      className={`flex h-fit items-center justify-center rounded-sm transition font-mediun ${icon && 'gap-1'} ${colorClasses[color]} ${sizeClasses[size]} ${className}`}
       {...props}
     >
       {iconPosition === "left" && <span>{icon}</span>}
