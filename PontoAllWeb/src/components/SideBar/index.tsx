84d8f2ffb0555de3bf4c82ec538670ca5112f0a8
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2025-09-05 17:02:03 -0300

    :lipstick: feat: Atualizando layout do componente sidebar

--- PontoAllWeb/src/components/SideBar/index.tsx
+++ PontoAllWeb/src/components/SideBar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { MdAccountCircle } from 'react-icons/md';
+import { MdAccountCircle, MdMenu, MdClose } from 'react-icons/md';
 
 import miniLogo from '../../assets/images/miniLogo.svg';
 import logoPontoAll from '../../assets/images/logoPontoAll.svg';
@@ -24,29 +24,29 @@ export default function SideBar() {
     <div
       onClick={() => setIsOpen(!isOpen)}
       className={`flex flex-col ${
-        isOpen ? 'w-60' : 'w-16'
+        isOpen ? 'w-48' : 'w-10'
       } min-h-screen bg-white shadow-lg transition-all duration-500 overflow-hidden cursor-pointer`}
     >
       <div className='relative h-16 flex items-center justify-center mb-6'>
-        <img
-          src={miniLogo}
-          alt='Logo Ícone'
-          className={`absolute left-1/2 -translate-x-1/2 transition-all duration-500 ease-in-out ${
-            isOpen
-              ? 'opacity-0 scale-75 -translate-y-2'
-              : 'opacity-100 scale-100 translate-y-0'
-          }`}
-          style={{ height: '2rem' }}
+        <MdMenu
+          className={`absolute left-1/2 -translate-x-1/2 transition-all duration-500 ease-in-out text-text-primary
+      ${
+        isOpen
+          ? 'opacity-0 scale-75 -translate-y-2'
+          : 'opacity-100 scale-100 translate-y-0'
+      }
+      text-primary`}
+          size={28}
         />
-        <img
-          src={logoPontoAll}
-          alt='Logo Completa'
-          className={`absolute left-1/2 -translate-x-1/2 transition-all duration-500 ease-in-out ${
-            isOpen
-              ? 'opacity-100 scale-100 translate-y-0'
-              : 'opacity-0 scale-75 -translate-y-2'
-          }`}
-          style={{ height: '3.5rem' }}
+        <MdClose
+          className={`absolute left-1/2 -translate-x-1/2 transition-all duration-500 ease-in-out text-text-primary
+      ${
+        isOpen
+          ? 'opacity-100 scale-100 translate-y-0'
+          : 'opacity-0 scale-75 -translate-y-2'
+      }
+      text-primary`}
+          size={28}
         />
       </div>
 
@@ -55,7 +55,7 @@ export default function SideBar() {
           <button
             key={button.id}
             onClick={() => navigate(button.route)}
-            className={`flex items-center gap-2 h-12 whitespace-nowrap border-x-4 text-primary ${
+            className={`flex items-center gap-2 h-12 whitespace-nowrap text-text-primary ${
               isOpen ? 'px-4' : 'justify-center'
             } ${
               location.pathname === button.route
