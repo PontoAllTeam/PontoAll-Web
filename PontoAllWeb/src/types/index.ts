b32b6d81339d5d8d57ed48db805a7be1b7a7dbfc
Larksel <lemuelgames@outlook.com>
2025-11-11 16:51:47 -0300

    :sparkles: feat: Reexportação dos tipos

---
+++ PontoAllWeb/src/types/index.ts
@@ -0,0 +1,20 @@
+// App types
+export * from './app/enum';
+export * from './app/geolocation';
+export * from './app/login';
+export * from './app/routeDefinition';
+export * from './app/serviceResult';
+
+// Enums
+export * from './enums/companyStatus';
+export * from './enums/scheduleDayType';
+export * from './enums/userStatus';
+export * from './enums/userType';
+
+// Models
+export * from './models/company';
+export * from './models/department';
+export * from './models/geofence';
+export * from './models/sector';
+export * from './models/user';
+export * from './models/workSchedule';
