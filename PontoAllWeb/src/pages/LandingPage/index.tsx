14d4ff02aad69e9b343397e0977da74f350af602
Larksel <lemuelgames@outlook.com>
2025-11-15 16:42:59 -0300

    :sparkles: feat: Botão da landing page redirecionando para login

--- PontoAllWeb/src/pages/LandingPage/index.tsx
+++ PontoAllWeb/src/pages/LandingPage/index.tsx
@@ -1,6 +1,11 @@
 import imgLangingPage1 from '@/assets/images/imgLandingPage1.svg';
+import useAppRoutes from '@/hooks/useAppRoutes';
+import { useNavigate } from 'react-router-dom';
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+  const routes = useAppRoutes();
+
   return (
     <div className='bg-background flex flex-col items-center'>
       <div className='flex justify-between items-center lg:px-13 lg:flex-row flex-col'>
@@ -12,7 +17,10 @@ export default function LandingPage() {
             Automatize jornadas, evite erros e ganhe controle total sobre a
             rotina da sua equipe.
           </p>
-          <button className='bg-secondary hover:bg-accent text-white font-semibold py-3 px-9 rounded-full transition duration-300 shadow-md'>
+          <button
+            className='bg-secondary hover:bg-accent text-white font-semibold py-3 px-9 rounded-full transition duration-300 shadow-md'
+            onClick={() => navigate(routes.LOGIN.path)}
+          >
             Contrate nosso serviço
           </button>
         </div>
