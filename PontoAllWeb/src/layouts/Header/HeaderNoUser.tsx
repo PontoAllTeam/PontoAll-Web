fd3e8e34cc8288e42a95fc86a0c9d66ae6ac0bca
Larksel <lemuelgames@outlook.com>
2025-11-18 16:41:27 -0300

    :lipstick: feat: Atualização no tamanho do header

--- PontoAllWeb/src/layouts/Header/HeaderNoUser.tsx
+++ PontoAllWeb/src/layouts/Header/HeaderNoUser.tsx
@@ -14,15 +14,15 @@ export default function Header() {
   return (
     <header className='w-full z-[5]'>
       <AccessibilityBar />
-      <div className='bg-white flex items-center justify-between h-24 shadow z-10 w-full'>
-        <div className='w-full mx-auto px-4 sm:px-6 lg:px-8 h-24 flex items-center justify-between'>
+      <div className='bg-white flex items-center justify-between h-12 shadow z-10 w-full'>
+        <div className='w-full px-4 sm:px-6 lg:px-8 h-full flex items-center justify-between'>
           <img
             src={logoPontoAll}
             alt='logo do sistema'
-            className='h-20 w-auto'
+            className='h-full object-cover'
           />
           <button
-            className='bg-secondary hover:bg-accent text-white text-md font-medium py-2 px-12 rounded-4xl transition duration-300 shadow-md'
+            className='bg-secondary hover:bg-accent text-white text-sm font-medium py-2 px-6 rounded-full transition duration-300 shadow-md'
             onClick={LoginPage}
           >
             Entrar
