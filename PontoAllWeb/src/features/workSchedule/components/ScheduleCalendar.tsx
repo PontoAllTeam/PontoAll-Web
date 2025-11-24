2734c8b94077626ad4053f8b64a5acf6ce073881
Larksel <lemuelgames@outlook.com>
2025-11-23 21:26:53 -0300

    :sparkles: feat: Calendário das escalas separado em um componente

---
+++ PontoAllWeb/src/features/workSchedule/components/ScheduleCalendar.tsx
@@ -0,0 +1,134 @@
+import { User, WorkSchedule } from '@/types';
+import { PiUserFill } from 'react-icons/pi';
+import ScheduleCard from './ScheduleCard';
+
+const WEEK_DAYS = [
+  { shortName: 'Seg', dayOfWeek: 1 },
+  { shortName: 'Ter', dayOfWeek: 2 },
+  { shortName: 'Qua', dayOfWeek: 3 },
+  { shortName: 'Qui', dayOfWeek: 4 },
+  { shortName: 'Sex', dayOfWeek: 5 },
+  { shortName: 'Sáb', dayOfWeek: 6 },
+  { shortName: 'Dom', dayOfWeek: 0 },
+];
+
+interface ScheduleCalendarProps {
+  users: User[];
+  workSchedules: WorkSchedule[];
+  currentWeek: Date;
+  onWeekChange: (newWeek: Date) => void;
+}
+
+export default function ScheduleCalendar({
+  users,
+  workSchedules,
+  currentWeek,
+  onWeekChange,
+}: ScheduleCalendarProps) {
+  const getWeekDates = () => {
+    const startOfWeek = new Date(currentWeek);
+    startOfWeek.setDate(currentWeek.getDate() - currentWeek.getDay() + 1);
+
+    return WEEK_DAYS.map((day, index) => {
+      const date = new Date(startOfWeek);
+      date.setDate(startOfWeek.getDate() + index);
+      return {
+        ...day,
+        date: date.getDate().toString().padStart(2, '0'),
+        month: (date.getMonth() + 1).toString().padStart(2, '0'),
+        fullDate: date,
+      };
+    });
+  };
+
+  const findScheduleForDate = (userId: number, date: Date) => {
+    const dayOfMonth = date.getDate();
+    const yearMonth = `${date.getFullYear()}/${(date.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}`;
+
+    return workSchedules.find(
+      (schedule) =>
+        schedule.userId === userId &&
+        schedule.dayOfMonth === dayOfMonth &&
+        schedule.yearMonth === yearMonth
+    );
+  };
+
+  const formattedData = users.map((user) => ({
+    ...user,
+    schedules: workSchedules.filter((schedule) => schedule.userId === user.id),
+  }));
+
+  return (
+    <div className='bg-white p-6 rounded-2xl h-full max-h-fit flex flex-col'>
+      {/* Cabeçalhos */}
+      <div className='grid grid-cols-[minmax(160px,_1.5fr)_repeat(7,_minmax(120px,_1fr))] border-b border-text-primary'>
+        <div className='col-span-1 flex items-center justify-between z-10'>
+          <h2 className='font-semibold text-text-primary text-sm'>
+            Colaboradores
+          </h2>
+          {/* Dias */}
+          <div className='flex items-center space-x-1'>
+            <button
+              onClick={() =>
+                onWeekChange(
+                  new Date(currentWeek.getTime() - 7 * 24 * 60 * 60 * 1000)
+                )
+              }
+              className='p-1 text-text-primary'
+            >
+              {'<'}
+            </button>
+            <button
+              onClick={() =>
+                onWeekChange(
+                  new Date(currentWeek.getTime() + 7 * 24 * 60 * 60 * 1000)
+                )
+              }
+              className='p-1 text-text-primary'
+            >
+              {'>'}
+            </button>
+          </div>
+        </div>
+        {getWeekDates().map(({ shortName, date, month }) => (
+          <div key={shortName} className='col-span-1 text-center'>
+            <span className='font-semibold text-text-primary text-sm'>
+              {`${shortName} ${date}/${month}`}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      {/* Conteúdo */}
+      {formattedData.map((employee) => (
+        <div
+          key={employee.id}
+          className='grid grid-cols-[minmax(160px,_1.5fr)_repeat(7,_minmax(120px,_1fr))] border-b border-text-primary'
+        >
+          {/* Nome do Colaborador */}
+          <div className='flex flex-row items-center justify-start shrink-0 gap-2'>
+            <PiUserFill className='text-text-secondary text-lg' />
+            <p className='text-text-secondary font-medium text-sm'>
+              {employee.name}
+            </p>
+          </div>
+
+          {getWeekDates().map((day) => {
+            const schedule = findScheduleForDate(employee.id, day.fullDate);
+
+            return (
+              <div
+                key={`${employee.id}-${day.date}`}
+                className='col-span-1 px-2 py-3'
+              >
+                <ScheduleCard workSchedule={schedule} />
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  );
+}
--- PontoAllWeb/src/features/workSchedule/pages/WorkScheduleOverview.tsx
+++ PontoAllWeb/src/features/workSchedule/pages/WorkScheduleOverview.tsx
@@ -1,7 +1,7 @@
 import BreadcrumbPageTitle from '@/components/BreadcrumbPageTitle';
-import ScheduleCard from '../components/ScheduleCard';
+import ScheduleCalendar from '../components/ScheduleCalendar';
 import { User, WorkSchedule } from '@/types';
-import { PiUserFill, PiPlus } from 'react-icons/pi';
+import { PiPlus } from 'react-icons/pi';
 import { useCallback, useEffect, useState } from 'react';
 import { AlertModal } from '@/components/Modal';
 import { UserService } from '@/features/user';
@@ -9,16 +9,6 @@ import WorkScheduleService from '../services/workScheduleService';
 import Button from '@/components/Button';
 import useAppRoutes from '@/hooks/useAppRoutes';
 import { useNavigate } from 'react-router-dom';
-
-const WEEK_DAYS = [
-  { shortName: 'Seg', dayOfWeek: 1 },
-  { shortName: 'Ter', dayOfWeek: 2 },
-  { shortName: 'Qua', dayOfWeek: 3 },
-  { shortName: 'Qui', dayOfWeek: 4 },
-  { shortName: 'Sex', dayOfWeek: 5 },
-  { shortName: 'Sáb', dayOfWeek: 6 },
-  { shortName: 'Dom', dayOfWeek: 0 },
-];
 
 export default function WorkScheduleOverview() {
   const routes = useAppRoutes();
@@ -61,41 +51,6 @@ export default function WorkScheduleOverview() {
     setIsAlertModalOpen(true);
   };
 
-  const getWeekDates = () => {
-    const startOfWeek = new Date(currentWeek);
-    startOfWeek.setDate(currentWeek.getDate() - currentWeek.getDay() + 1);
-
-    return WEEK_DAYS.map((day, index) => {
-      const date = new Date(startOfWeek);
-      date.setDate(startOfWeek.getDate() + index);
-      return {
-        ...day,
-        date: date.getDate().toString().padStart(2, '0'),
-        month: (date.getMonth() + 1).toString().padStart(2, '0'),
-        fullDate: date,
-      };
-    });
-  };
-
-  const findScheduleForDate = (userId: number, date: Date) => {
-    const dayOfMonth = date.getDate();
-    const yearMonth = `${date.getFullYear()}/${(date.getMonth() + 1)
-      .toString()
-      .padStart(2, '0')}`;
-
-    return workSchedules.find(
-      (schedule) =>
-        schedule.userId === userId &&
-        schedule.dayOfMonth === dayOfMonth &&
-        schedule.yearMonth === yearMonth
-    );
-  };
-
-  const formattedData = users.map((user) => ({
-    ...user,
-    schedules: workSchedules.filter((schedule) => schedule.userId === user.id),
-  }));
-
   return (
     <div className='overflow-clip'>
       <BreadcrumbPageTitle title='Escala de Trabalho' />
@@ -113,76 +68,12 @@ export default function WorkScheduleOverview() {
         <hr className='border-t border-neutral-dark' />
       </div>
       <div className='p-11 h-full'>
-        {/* Calendário */}
-        <div className='bg-white p-6 rounded-2xl h-full max-h-fit flex flex-col'>
-          {/* Cabeçalhos */}
-          <div className='grid grid-cols-[minmax(160px,_1.5fr)_repeat(7,_minmax(120px,_1fr))] border-b border-text-primary'>
-            <div className='col-span-1 flex items-center justify-between z-10'>
-              <h2 className='font-semibold text-text-primary text-sm'>
-                Colaboradores
-              </h2>
-              {/* Dias */}
-              <div className='flex items-center space-x-1'>
-                <button
-                  onClick={() =>
-                    setCurrentWeek(
-                      new Date(currentWeek.getTime() - 7 * 24 * 60 * 60 * 1000)
-                    )
-                  }
-                  className='p-1 text-text-primary'
-                >
-                  {'<'}
-                </button>
-                <button
-                  onClick={() =>
-                    setCurrentWeek(
-                      new Date(currentWeek.getTime() + 7 * 24 * 60 * 60 * 1000)
-                    )
-                  }
-                  className='p-1 text-text-primary'
-                >
-                  {'>'}
-                </button>
-              </div>
-            </div>
-            {getWeekDates().map(({ shortName, date, month }) => (
-              <div key={shortName} className='col-span-1 text-center'>
-                <span className='font-semibold text-text-primary text-sm'>
-                  {`${shortName} ${date}/${month}`}
-                </span>
-              </div>
-            ))}
-          </div>
-
-          {/* Conteúdo */}
-          {formattedData.map((employee) => (
-            <div
-              key={employee.id}
-              className='grid grid-cols-[minmax(160px,_1.5fr)_repeat(7,_minmax(120px,_1fr))] border-b border-text-primary'
-            >
-              {/* Nome do Colaborador */}
-              <div className='flex flex-row items-center justify-start shrink-0 gap-2'>
-                <PiUserFill className='text-text-secondary text-lg' />
-                <p className='text-text-secondary font-medium text-sm'>
-                  {employee.name}
-                </p>
-              </div>
-
-              {getWeekDates().map((day) => {
-                const schedule = findScheduleForDate(employee.id, day.fullDate);
-
-                return (
-                  <div
-                    key={`${employee.id}-${day.date}`}
-                    className='col-span-1 px-2 py-3'
-                  >
-                    <ScheduleCard workSchedule={schedule} />
-                  </div>
-                );
-              })}
-            </div>
-          ))}
-        </div>
+        <ScheduleCalendar
+          users={users}
+          workSchedules={workSchedules}
+          currentWeek={currentWeek}
+          onWeekChange={setCurrentWeek}
+        />
       </div>
       <AlertModal
         isOpen={isAlertModalOpen}
